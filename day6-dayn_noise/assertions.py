322f75ec9816f9e7fc152e00bed957dcc5bf67b3
Jasraj Singh <jasraj001@e.ntu.edu.sg>
2022-06-17 18:15:22 +0800

    added C

--- day6-dayn_noise/assertions.py
+++ day6-dayn_noise/assertions.py
@@ -1,5 +1,10 @@
 'Boundary definitions for letters'
 
+from math import pi, tan
+
+y_lim, width = 5, 1
+left_end, right_end = -4, 4
+
 def get_xy(point):
 
     x, y = point
@@ -7,39 +12,48 @@ def get_xy(point):
 
     return x, y
 
-def assert_A(point):
+def assert_A(point, slope=10/3):
 
     x, y = get_xy(point)
 
-    inside_outer_left_arm    = lambda x, y: y < 10  * (x+3) / 3.5
-    inside_outer_right_arm   = lambda x, y: y < -10 * (x-3) / 3.5
-    outside_inner_left_arm   = lambda x, y: y > 10  * (x+2) / 3.5
-    outside_inner_right_arm  = lambda x, y: y > -10 * (x-2) / 3.5
+    inside_outer_left_arm    = lambda x, y: y + y_lim < slope  * (x - left_end) 
+    inside_outer_right_arm   = lambda x, y: y + y_lim < -slope * (x - right_end) 
+    outside_inner_left_arm   = lambda x, y: y + y_lim > slope  * (x - (left_end+width)) 
+    outside_inner_right_arm  = lambda x, y: y + y_lim > -slope * (x - (right_end-width)) 
 
-    return (-1 <= y <= 0 or 4 <= y <= 5)  and (inside_outer_left_arm(x, y) and inside_outer_right_arm(x, y)) \
-        or (-5 <= y <= -1 or 0 <= y <= 4) and (inside_outer_left_arm(x, y) and outside_inner_left_arm(x, y) or inside_outer_right_arm(x, y) and outside_inner_right_arm(x, y))
+    return (-width <= y <= 0 or y_lim-width <= y <= y_lim)  and (inside_outer_left_arm(x, y) and inside_outer_right_arm(x, y)) \
+        or (-y_lim <= y <= -width or 0 <= y <= y_lim-width) and (inside_outer_left_arm(x, y) and outside_inner_left_arm(x, y) or inside_outer_right_arm(x, y) and outside_inner_right_arm(x, y))
 
 def assert_B(point):
 
     x, y = get_xy(point)
-    return assert_D((x, 2*y-4.5)) or assert_D((x, 2*y+4.5)) 
 
+    return assert_D((x, 2*y - (y_lim - width/2))) \
+        or assert_D((x, 2*y + (y_lim - width/2))) 
+
+def assert_C(point, center=1, theta=pi/3):
+
+    x, y = get_xy(point)
+
+    return  (x-center)**2 + y**2 <= y_lim**2 \
+        and (x-center)**2 + y**2 >= (y_lim-1)**2 \
+        and (x < 0 or abs(y/(x-center)) > tan(theta))
 
 def assert_D(point):
 
     x, y = get_xy(point)
-    left_end, right_end, width = -4, 4, 1.5
 
     curve_left_end = left_end + width
     outer_major_axis = right_end - curve_left_end
-    outer_minor_axis = 5
+    outer_minor_axis = y_lim
     inner_major_axis = outer_major_axis - width 
     inner_minor_axis = outer_minor_axis - width
 
-    inside_outer_curve = lambda x, y:  outer_minor_axis**2 * (x-curve_left_end)**2 + outer_major_axis**2 * y**2 <= outer_minor_axis**2 * outer_major_axis**2 
+    inside_outer_curve  = lambda x, y: outer_minor_axis**2 * (x-curve_left_end)**2 + outer_major_axis**2 * y**2 <= outer_minor_axis**2 * outer_major_axis**2 
     outside_inner_curve = lambda x, y: inner_minor_axis**2 * (x-curve_left_end)**2 + inner_major_axis**2 * y**2 >= inner_minor_axis**2 * inner_major_axis**2
 
-    return (-outer_minor_axis <= y <= outer_major_axis) and (left_end <= x <= left_end+width or left_end+width <= x <= right_end and inside_outer_curve(x, y) and outside_inner_curve(x, y))
+    return (-outer_minor_axis <= y <= outer_major_axis) and (left_end <= x <= left_end+width or \
+        left_end+width <= x <= right_end and inside_outer_curve(x, y) and outside_inner_curve(x, y))
 
 if __name__ == '__main__':
 
@@ -57,6 +71,8 @@ if __name__ == '__main__':
         assert_func = assert_A
     elif letter == 'B':
         assert_func = assert_B
+    elif letter == 'C':
+        assert_func = assert_C
     elif letter == 'D':
         assert_func = assert_D
     else:
@@ -68,6 +84,6 @@ if __name__ == '__main__':
     sample = [point for point in sample if assert_func(point)]
 
     plt.scatter(*zip(*sample))
-    plt.xlim(-5, 5)
-    plt.ylim(-5, 5)
+    plt.xlim(-y_lim, y_lim)
+    plt.ylim(-y_lim, y_lim)
     plt.show()
